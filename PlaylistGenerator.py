698b04ed1ac3700611d10db775b36579c807a195
niboeing <32905101+breeeeeeeze@users.noreply.github.com>
2022-04-21 01:44:15 +0200

    some improvements

--- PlaylistGenerator.py
+++ PlaylistGenerator.py
@@ -1,12 +1,12 @@
 from datetime import date
+from tqdm import tqdm
 import SimpleLogger as logger
 import Utils
-import sys, pprint
-from tqdm import tqdm
 
 class PlaylistGenerator:
-	def __init__(self, Spotify, playlistURI=None, country='DE', days=7, config = None):
-		self.Spotify = Spotify
+	'''Playlist generator base class'''
+	def __init__(self, spotify, playlistURI=None, country='DE', days=7, config=None):
+		self.spotify = spotify
 		self.playlistURI = playlistURI
 		self.playlistTracks = []
 		self.checkedAlbums = []
@@ -20,43 +20,45 @@ class PlaylistGenerator:
 
 
 	def getUserLikedTracks(self):
-		logger.log('Getting list of liked tracks', 'info')
-		count = 50
-		results = self.Spotify.current_user_saved_tracks(limit = 50)
+		logger.log('Fetching list of liked tracks', 'info')
+		results = self.spotify.current_user_saved_tracks(limit = 50)
 		self.userLikedTracksIds.extend(results['items'])
-		#pprint.pprint(self.userLikedTracksIds[:3])
-		#sys.exit()
 		while results['next']:
-			results = self.Spotify.next(results)
+			results = self.spotify.next(results)
 			self.userLikedTracksIds.extend(results['items'])
 		self.userLikedTracksIds = [track['track']['id'] for track in self.userLikedTracksIds]
 
 	def resetPlaylist(self):
-		self.Spotify.playlist_replace_items(self.playlistURI, ['spotify:track:4RWkW7tGWseUu1T9LzpEBP'])
-		self.Spotify.playlist_remove_all_occurrences_of_items(self.playlistURI, ['spotify:track:4RWkW7tGWseUu1T9LzpEBP'])
+		self.spotify.playlist_replace_items(self.playlistURI, ['spotify:track:4RWkW7tGWseUu1T9LzpEBP'])
+		self.spotify.playlist_remove_all_occurrences_of_items(self.playlistURI, ['spotify:track:4RWkW7tGWseUu1T9LzpEBP'])
 
 	def checkAlbum(self, album, label=None):
-		if ( album['release_date_precision'] == 'day' and
-			not Utils.isRadioshow(album['name']) and
-			abs((self.currentDate - date.fromisoformat(album['release_date'])).days) <= self.daysSinceRelease and
-			album['id'] not in self.checkedAlbums):
-			album_extended = self.Spotify.album(album['id'])
-			if ( self.country in album_extended['available_markets'] and
-				(not label or label == album_extended['label'])):
+		if (album['release_date_precision'] == 'day'
+				and not Utils.isRadioshow(album['name'])
+				and abs((self.currentDate - date.fromisoformat(album['release_date'])).days) <= self.daysSinceRelease
+				and album['id'] not in self.checkedAlbums):
+			try:
+				albumExtended = self.spotify.album(album['id'])
+			except Exception:
+				logger.log('Could not fetch album ' + album['name'] + ' (' + album['id'] + ')', 'error')
+				return False
+			if (self.country in albumExtended['available_markets'] and
+				(not label or label == albumExtended['label'])):
 				self.checkedAlbums.append(album['id'])
 				return True
 		return False
 
 	def getArtistList(self):
-		logger.log('Getting list of followed artists','info')
-		results = self.Spotify.current_user_followed_artists()
+		logger.log('Fetching list of followed artists','info')
+		results = self.spotify.current_user_followed_artists()
 		self.artistList.extend(results['artists']['items'])
 		while results['artists']['next']:
-			results = self.Spotify.next(results['artists'])
+			results = self.spotify.next(results['artists'])
 			self.artistList.extend(results['artists']['items'])
 
 	def isDuplicate(self, track):
-		if not self.config['IGNORE_DUPLICATES']: return False
+		if not self.config['IGNORE_DUPLICATES']:
+			return False
 		artists = ','.join(el['name'] for el in track['artists'])
 		name = track['name']
 		trackDict  = {artists: name}
@@ -66,39 +68,35 @@ class PlaylistGenerator:
 		return False
 
 	def isLiked(self, track):
-		if not self.config['IGNORE_ALREADY_LIKED']: return False
+		if not self.config['IGNORE_ALREADY_LIKED']:
+			return False
 		if track['id'] in self.userLikedTracksIds:
 			return True
 		return False
 
 	def artistIsFollowed(self, track):
-		if not self.config['IGNORE_LABEL_IF_ARTIST_FOLLOWED']: return False
+		if not self.config['IGNORE_LABEL_IF_ARTIST_FOLLOWED']:
+			return False
 		artistFollowedIds = [artist['id'] for artist in self.artistList]
 		for artist in track['artists']:
 			if artist['id'] in artistFollowedIds:
 				return True
 		return False
 
-	def checkTrack(self, track, artist=None):
-		for a in track['artists']:
-			if (not artist or artist['name'] == a['name']) and not Utils.isExtended(track['name']):
-				if (not self.isDuplicate(track) and not self.isLiked(track)):
-					return True
-		return False
-
-	def checkTrack2(self, track, artist=None):
-		for a in track['artists']:
-			if (not artist or artist['name'] == a['name']) and not Utils.isExtended(track['name']):
-				if (not self.isDuplicate(track) and not self.isLiked(track) and not self.artistIsFollowed(track)):
+	def checkTrack(self, track, artist=None, filterFollowedArtists=False):
+		for trackArtist in track['artists']:
+			if (not artist or artist['name'] == trackArtist['name']) and not Utils.isExtended(track['name']):
+				if (not self.isDuplicate(track)
+						and not self.isLiked(track)
+						and (not filterFollowedArtists or not self.artistIsFollowed(track))):
 					return True
 		return False
 
 	def addToPlaylistCache(self, track):
 		self.playlistTracks.append(track['id'])
-		track_artists = []
-		for a in track['artists']:
-			track_artists.append(a['name'])
-		#logger.log(f'{", ".join(track_artists)} - {track["name"]} added', 'debug')
+		trackArtists = []
+		for trackArtist in track['artists']:
+			trackArtists.append(trackArtist['name'])
 
 	def chunkTrackList(self):
 		for i in range(0, len(self.playlistTracks), 50):
@@ -108,20 +106,21 @@ class PlaylistGenerator:
 		logger.log('Writing playlist', 'info')
 		self.resetPlaylist()
 		chunkedList = list(self.chunkTrackList())
-		for el in chunkedList:
-			self.Spotify.playlist_add_items(self.playlistURI, el)
-							
+		for chunk in chunkedList:
+			self.spotify.playlist_add_items(self.playlistURI, chunk)
+
 class ArtistRecentTracks(PlaylistGenerator):
+	'''Playlist generator for recent tracks of an artist'''
 	def __init__(self, Spotify, playlistURI=None, albumTypes='album,single', country='DE', days=7, config=None):
 		super().__init__(Spotify, playlistURI=playlistURI, country=country, days=days, config=config)
 		self.albumTypes = albumTypes
 
 	def getArtistAlbums(self, artistID):
 		artistAlbumList = []
-		results = self.Spotify.artist_albums(artistID, limit=50, album_type=self.albumTypes, country=self.country)
+		results = self.spotify.artist_albums(artistID, limit=50, album_type=self.albumTypes, country=self.country)
 		artistAlbumList.extend(results['items'])
 		while results['next']:
-			results = self.Spotify.next(results)
+			results = self.spotify.next(results)
 			artistAlbumList.extend(results['items'])
 		return artistAlbumList
 
@@ -129,19 +128,20 @@ class ArtistRecentTracks(PlaylistGenerator):
 		self.getArtistList()
 		self.getUserLikedTracks()
 		logger.log('Finding recently released tracks','info')
-		lelel = tqdm(self.artistList, leave=True)
+		lelel = tqdm(self.artistList, leave=True, disable=self.config['DISABLE_PROGRESS_BAR'])
 		for artist in lelel:
 			lelel.set_postfix_str(artist['name'], refresh='True')
 			artistAlbumList = self.getArtistAlbums(artist['id'])
 			for album in artistAlbumList:
 				if self.checkAlbum(album):
-					tracks = self.Spotify.album_tracks(album['id'])
+					tracks = self.spotify.album_tracks(album['id'])
 					for track in tracks['items']:
 						if self.checkTrack(track, artist):
 							self.addToPlaylistCache(track)
 		self.writePlaylist()
 
 class LabelRecentTracks(PlaylistGenerator):
+	'''Playlist generator for recent tracks of a label'''
 	def __init__(self, Spotify, labels, playlistURI=None, country='DE', days=7, config=None):
 		super().__init__(Spotify, playlistURI=playlistURI, country=country, days=days, config=config)
 		if isinstance(labels, str):
@@ -149,28 +149,27 @@ class LabelRecentTracks(PlaylistGenerator):
 		self.labelList = labels
 
 	def findLabelReleases(self, label):
-		#logger.log(f'Finding albums for {label}', 'info')
 		labelAlbumList = []
-		results = self.Spotify.search(q=f'label:"{label.lower().replace(" ","+")}" tag:new', limit=10, type='album')['albums']
+		results = self.spotify.search(q=f'label:"{label.lower().replace(" ","+")}" tag:new', limit=10, type='album')['albums']
 		labelAlbumList.extend(results['items'])
 		while results['next']:
-			results = self.Spotify.next(results)['albums']
+			results = self.spotify.next(results)['albums']
 			labelAlbumList.extend(results['items'])
 		return labelAlbumList
-			
+
 
 	def run(self):
 		self.getArtistList()
 		self.getUserLikedTracks()
 		logger.log('Collecting releases','info')
-		tqdm_label = tqdm(self.labelList, leave=True)
-		for label in tqdm_label:
-			tqdm_label.set_postfix_str(label, refresh="True")
+		tqdmLabel = tqdm(self.labelList, leave=True, disable=self.config['DISABLE_PROGRESS_BAR'])
+		for label in tqdmLabel:
+			tqdmLabel.set_postfix_str(label, refresh="True")
 			labelAlbumList = self.findLabelReleases(label)
 			for album in labelAlbumList:
 				if self.checkAlbum(album, label=label):
-					tracks = self.Spotify.album_tracks(album['id'])
+					tracks = self.spotify.album_tracks(album['id'])
 					for track in tracks['items']:
-						if self.checkTrack2(track):
+						if self.checkTrack(track, filterFollowedArtists = True):
 							self.addToPlaylistCache(track)
 		self.writePlaylist()
--- main.py
+++ main.py
@@ -1,34 +1,58 @@
+import time
+import traceback
+import sys
+import configparser as cfg
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import SimpleLogger as logger
-import time,traceback,sys
 from PlaylistGenerator import ArtistRecentTracks, LabelRecentTracks
-import configparser as cfg
 
-config = cfg.ConfigParser()
-config.read('config.ini')
+def main():
+	config = cfg.ConfigParser()
+	config.read('config.ini')
 
-time_start = time.time()
-playlistURIArtist = config['General']['PLAYLIST_URI_ARTISTS']
-playlistURILabels = config['General']['PLAYLIST_URI_LABELS']
-albumTypes = config['General']['ALBUM_TYPES']
-country = config['General']['REGION']
-days = int(config['General']['DAYS'])
-with open(config['General']['LABELS_FILE'],'r') as f:
-	labelList = f.read().splitlines()
+	timeStart = time.time()
+	playlistURIArtist = config['General']['PLAYLIST_URI_ARTISTS']
+	playlistURILabels = config['General']['PLAYLIST_URI_LABELS']
+	albumTypes = config['General']['ALBUM_TYPES']
+	country = config['General']['REGION']
+	days = int(config['General']['DAYS'])
+	with open(config['General']['LABELS_FILE'],'r') as inFile:
+		labelList = inFile.read().splitlines()
 
-try:
-	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config['Spotify']['CLIENT_ID'], client_secret=config['Spotify']['CLIENT_SECRET'], redirect_uri=config['Spotify']['REDIRECT_URI'], scope=config['Spotify']['SCOPE'], username='bxtmusic'))
+	try:
+		spotifyClient = spotipy.Spotify(auth_manager=SpotifyOAuth(
+											client_id=config['Spotify']['CLIENT_ID'],
+											client_secret=config['Spotify']['CLIENT_SECRET'],
+											redirect_uri=config['Spotify']['REDIRECT_URI'],
+											scope=config['Spotify']['SCOPE'],
+											username='bxtmusic'))
 
-	labels = LabelRecentTracks(sp, labelList, playlistURI=playlistURILabels, country=country, days=days, config = config['General'])
-	labels.run()
+		labels = LabelRecentTracks(
+									spotifyClient,
+									labelList,
+									playlistURI=playlistURILabels,
+									country=country,
+									days=days,
+									config = config['General']
+								)
+		labels.run()
 
-	artists = ArtistRecentTracks(sp, playlistURI=playlistURIArtist, albumTypes=albumTypes, country=country, days=days, config = config['General'])
-	artists.run()
+		artists = ArtistRecentTracks(
+										spotifyClient,
+										playlistURI=playlistURIArtist,
+										albumTypes=albumTypes,
+										country=country,
+										days=days,
+										config = config['General']
+									)
+		artists.run()
 
-except Exception:
-	logger.log(traceback.format_exc(), 'error')
-	sys.exit(1)
+	except Exception:
+		logger.log(traceback.format_exc(), 'error')
+		sys.exit(1)
 
-logger.log(f'Time taken: {time.time() - time_start}', 'info')
+	logger.log(f'Time taken: {time.time() - timeStart}', 'info')
 
+if __name__ == '__main__':
+	main()
